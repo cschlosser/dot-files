ad1eeab35f49cd05f9cee667a79379b17f516d94
Christoph Schlosser <christoph.schlosser@bshg.com>
2021-03-22 12:18:35 +0100

    Add polybar

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -1,8 +1,7 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH $HOME/.config/omf/path
-
-# Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG $HOME/.config/omf/config
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
---
+++ herbstluft/panel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+polybar -c $HOME/.config/polybar/config herbstluft
